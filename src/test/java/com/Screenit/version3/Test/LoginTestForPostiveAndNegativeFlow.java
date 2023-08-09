f20b5a84d5a3053f47f36bf1a9ceec1a10b918ac
vipersathish <sathishshan156@gmail.com>
2023-08-09 17:02:47 +0530

    first commit

--- src/test/java/com/Screenit/version3/Test/LoginTestForPostiveAndNegativeFlow.java
+++ src/test/java/com/Screenit/version3/Test/LoginTestForPostiveAndNegativeFlow.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.concurrent.TimeUnit;
-
 import org.apache.commons.io.FileUtils;
 
 import org.openqa.selenium.By;
